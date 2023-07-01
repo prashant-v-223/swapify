db2e0e4d47aecc9ee41fda326c0056daf2e90a74
anshu sharma <anshusharma6327@gmail.com>
2023-07-01 19:41:10 +0530

    intial ui

--- src/components/Deposit.tsx
+++ src/components/Deposit.tsx
@@ -4,7 +4,7 @@ const Deposit = () => {
   return (
     <div className="p-4">
     <div className="md:hidden block">
-    <iframe title="Exolix widget" src="https://exolix.com/widget/BTC:BTC-ETH:ETH?a=1&locale=en&t=pEtLANcgIp5bfll5kbTOe7H0U2zyATdcjcOYyTCrztCPK8KklAHASO4quKtbDC0s" width="360px" height="376px" frameBorder="0" scrolling="yes"></iframe>
+    <iframe title="Exolix widget" src="https://exolix.com/widget/BTC:BTC-ETH:ETH?a=1&locale=en&t=pEtLANcgIp5bfll5kbTOe7H0U2zyATdcjcOYyTCrztCPK8KklAHASO4quKtbDC0s" width="300px" height="376px" frameBorder="0" scrolling="yes"></iframe>
     </div>
       <div className="md:block hidden">
       <iframe title="Exolix widget" src="https://exolix.com/widget/BTC:BTC-ETH:ETH?a=1&locale=en&t=pEtLANcgIp5bfll5kbTOe7H0U2zyATdcjcOYyTCrztCPK8KklAHASO4quKtbDC0s" width="560px" height="376px" frameBorder="0" scrolling="yes"></iframe>
