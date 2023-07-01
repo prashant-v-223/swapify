184e2d2d89a1ba67813a7fe8d77bc6c40802afeb
anshu sharma <anshusharma6327@gmail.com>
2023-07-01 19:33:56 +0530

    intial ui

--- src/components/Deposit.tsx
+++ src/components/Deposit.tsx
@@ -3,9 +3,7 @@ import TransactionTable from './TransactionTable'
 const Deposit = () => {
   return (
     <div className="p-4">
-    <div className="flex w-full justify-center">
     <iframe title="Exolix widget" src="https://exolix.com/widget/BTC:BTC-ETH:ETH?a=1&locale=en&t=pEtLANcgIp5bfll5kbTOe7H0U2zyATdcjcOYyTCrztCPK8KklAHASO4quKtbDC0s" width="560px" height="376px" frameBorder="0" scrolling="yes"></iframe>
-    </div>
       <TransactionTable />
     </div>
   )
